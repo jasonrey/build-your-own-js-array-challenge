0bf32ba4ba46ed6984d22a2a63fce597ee7008a8
JSON object <jsonobject@outlook.com>
2017-11-29 09:18:13 +0800

    Update tests

--- test/level-4/reduce.js
+++ test/level-4/reduce.js
@@ -22,3 +22,50 @@ test('reduce() throws error if non function type is passed in', t => {
   t.throws(() => array.reduce('hello'), Error)
   t.throws(() => array.reduce(null), Error)
 })
+
+test('reduce(fn) throws error if no initial value provided and empty array', t => {
+  const array = new CustomArray()
+
+  t.throws(() => array.reduce(() => {}), Error)
+})
+
+test('reduce(fn) uses value of first element on single element array as accumulator', t => {
+  const array = new CustomArray()
+
+  array.push(1)
+
+  array.reduce((acc, item) => {
+    t.is(acc, 1)
+  })
+})
+
+test('reduce(fn) does not mutate the array', t => {
+  const array = new CustomArray()
+
+  array.push(1)
+
+  array.reduce((acc, item) => acc)
+
+  t.is(array.length, 1)
+  t.is(array[0], 1)
+})
+
+test('reduce(fn, init) returns the accumulator', t => {
+  const array = new CustomArray()
+
+  array.push(1, 2, 3, 4, 5)
+
+  const result = array.reduce((acc, item) => acc + item, 0)
+
+  t.is(result, 15)
+})
+
+test('reduce(fn, init) returns the accumulator from left', t => {
+  const array = new CustomArray()
+
+  array.push(1, 2, 3, 4, 5)
+
+  const result = array.reduce((acc, item) => acc + item, '')
+
+  t.is(result, '12345')
+})
--- test/level-4/reduceRight.js
+++ test/level-4/reduceRight.js
@@ -22,3 +22,50 @@ test('reduceRight() throws error if non function type is passed in', t => {
   t.throws(() => array.reduceRight('hello'), Error)
   t.throws(() => array.reduceRight(null), Error)
 })
+
+test('reduceRight(fn) throws error if no initial value provided and empty array', t => {
+  const array = new CustomArray()
+
+  t.throws(() => array.reduceRight(() => {}), Error)
+})
+
+test('reduceRight(fn) uses value of first element on single element array as accumulator', t => {
+  const array = new CustomArray()
+
+  array.push(1)
+
+  array.reduceRight((acc, item) => {
+    t.is(acc, 1)
+  })
+})
+
+test('reduceRight(fn) does not mutate the array', t => {
+  const array = new CustomArray()
+
+  array.push(1)
+
+  array.reduceRight((acc, item) => acc)
+
+  t.is(array.length, 1)
+  t.is(array[0], 1)
+})
+
+test('reduceRight(fn, init) returns the accumulator', t => {
+  const array = new CustomArray()
+
+  array.push(1, 2, 3, 4, 5)
+
+  const result = array.reduceRight((acc, item) => acc + item, 0)
+
+  t.is(result, 15)
+})
+
+test('reduceRight(fn, init) returns the accumulator from left', t => {
+  const array = new CustomArray()
+
+  array.push(1, 2, 3, 4, 5)
+
+  const result = array.reduceRight((acc, item) => acc + item, '')
+
+  t.is(result, '54321')
+})
